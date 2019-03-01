67075d8a8d95ea68868e6ba1670b0b251a282af1
pavkakhanov <pavkahanov@gmail.com>
2019-03-01 17:09:19 +0700

    Added: + Checking active vote based on blocks + Posibility to check is values correct + Prevent voting, if user already vote
    Fixed:
      - vote time moved from vote struct to question struct

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -20,24 +20,33 @@ contract Voting {
     uint256 questionId;
     uint starterGroup;
     address starterAddr;
+    uint start;
     uint result;
     mapping (address => bool) voters;
+    mapping (address => bool) votedUsers;
     mapping (bool => uint256) votes;
   }
 //
   mapping (uint => question) public _questions;
-  mapping (uint => voting) public _votings;
+  mapping (uint=> voting) public _votings;
   mapping (bool => uint) public _votes;
 
   
   function startVote(uint256 questionId, uint starterGroup) public returns (bool) {
-    _votings[_voteId].questionId = questionId;
-    _votings[_voteId].starterGroup = starterGroup;
-    _votings[_voteId].starterAddr = msg.sender;
-
-    _voteId += 1;
+    bool isActiveVote = isVoting();
+    if (isActiveVote){
+        _votings[_voteId].questionId = questionId;
+        _votings[_voteId].starterGroup = starterGroup;
+        _votings[_voteId].starterAddr = msg.sender;
+        _votings[_voteId].start = block.number;
+    
+        _voteId += 1; 
+    } else {
+        return false;
+    }
+    
   }
-  
+
   event voteNotification(uint votingId, address voter, bool descision, uint256 voteWeight);
 
   function addQuestion(uint status, string memory caption, string memory text, uint groupId, uint time) public {
@@ -52,21 +61,46 @@ contract Voting {
   }
 
 // rename to setVote 
-  function setVote(uint votingId, address voter, bool descision, uint256 voteWeight) public returns(uint, address, bool, uint256) {
-    _votings[votingId].voters[voter] = descision;
-    _votings[votingId].votes[descision] += voteWeight;
-    _votes[descision] += voteWeight;
+  function setVote(uint votingId, address voter, bool descision, uint256 voteWeight) public returns(bool) {
+      
+    if (_votings[votingId].votedUsers[voter] == false){
+        _votings[votingId].voters[voter] = descision;
+        _votings[votingId].votes[descision] += voteWeight;
+        _votes[descision] += voteWeight;
+        _votings[votingId].votedUsers[voter] = true;
+        
+    } else if (_votings[votingId].votedUsers[voter] == true){
+        
+        return false;
+    }
+
 
     emit voteNotification(votingId, voter, descision, voteWeight);
   }
   
-  function getQuestion(uint questionId) public returns (string memory, string memory) {
-    return(_questions[questionId].caption, _questions[questionId].text);
+  function getUserVote(uint votingId, address user) public view returns (bool) {
+    return _votings[votingId].voters[user];
+  }
+  function isUserVoted(address user, uint voteId) public view returns(bool){
+      return _votings[voteId].votedUsers[user];
+  }
+  function getDescisionWeight(bool descision) public returns (uint){
+    return _votes[descision];
   }
 
   function isVoting() public view returns (bool) {
-    uint number = uint(keccak256(abi.encodePacked(msg.sender, blockhash(block.number - 1))))%2;
-    bool isVote = number == 1 ? true : false;
+    uint questionId = _votings[_voteId-1].questionId;
+
+    uint endBlock = _questions[questionId].time + _votings[_voteId-1].start;
+
+    bool isVote;
+    if (endBlock >= block.number){
+      isVote = true;
+    } else {
+      isVote = false;
+    }
+    //uint number = uint(keccak256(abi.encodePacked(msg.sender, blockhash(block.number - 1))))%2;
+    //bool isVote = number == 1 ? true : false;
     return isVote;
   }
 
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -4,7 +4,7 @@ var Voting = artifacts.require("./Voting.sol");
 
 module.exports = function(deployer) {
   const variables = ["Test Token", "TST", 1000000]
-  const M_variables = ["Test", "MTT", '0x9C4d99880D2BEB018C7f4AD3b7a2FD30350a9A29', '0xAC8E686D8b9b78B6575273557a87e00B3d37fA98']
+  const M_variables = ["Test", "MTT", "0x9C4d99880D2BEB018C7f4AD3b7a2FD30350a9A29", "0xAC8E686D8b9b78B6575273557a87e00B3d37fA98"]
   deployer.deploy(ERC, variables[0], variables[1], variables[2],);
   deployer.deploy(MERC, M_variables[0], M_variables[1], M_variables[2], M_variables[3]);
   deployer.deploy(Voting);
